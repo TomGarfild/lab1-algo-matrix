21161304ca81a10e3cf584d3d03d039b5995b330
TomGarfild <safronuyk.allex73@gmail.com>
2021-10-30 10:22:30 +0300

    Add timer and deleting memory

--- Lab1-algo-matrix/Lab1-algo-matrix/main.cpp
+++ Lab1-algo-matrix/Lab1-algo-matrix/main.cpp
@@ -1,7 +1,25 @@
+#include <chrono>
 #include <iostream>
 using namespace std;
 
-const int LIMIT_WHEN_USE_MULTIPLY = 1;
+const int LIMIT_WHEN_USE_MULTIPLY = 64;
+const double EPSILON = 1e-6;
+
+bool twoMatrixEqual(double** a, double** b, int r, int c)
+{
+	for (int i = 0; i < r; i++)
+	{
+		for (int j = 0; j < c; j++)
+		{
+			if (fabs(a[i][j]-b[i][j]) > EPSILON)
+			{
+                return false;
+			}
+		}
+	}
+
+    return true;
+}
 
 void printMatrix(double** matrix, int r, int c = -1)
 {
@@ -19,8 +37,7 @@ void printMatrix(double** matrix, int r, int c = -1)
 
 double getRandNumber()
 {
-	//return (rand() % 100 + 1) / 10.0;
-    return (rand() % 10 + 1);
+	return (rand() % 100 + 1) / 10.0;
 }
 
 int log2(int x) {
@@ -178,18 +195,35 @@ double** multiplyStrassen(double** a, double** b, int size) {
     double** p6 = multiplyStrassen(sumOrSubMatrix(a21, a11, new_size, '-'), sumOrSubMatrix(b11, b12, new_size, '+'), new_size);
     double** p7 = multiplyStrassen(sumOrSubMatrix(a12, a22, new_size, '-'), sumOrSubMatrix(b21, b22, new_size, '+'), new_size);
 
+    delete[] a11;
+    delete[] a12;
+    delete[] a21;
+    delete[] a22;
+    delete[] b11;
+    delete[] b12;
+    delete[] b21;
+    delete[] b22;
+	
     double** c11 = sumOrSubMatrix(sumOrSubMatrix(p1, p4, new_size, '+'), sumOrSubMatrix(p7, p5, new_size, '-'), new_size, '+');
     double** c12 = sumOrSubMatrix(p3, p5, new_size, '+');
     double** c21 = sumOrSubMatrix(p2, p4, new_size, '+');
     double** c22 = sumOrSubMatrix(sumOrSubMatrix(p1, p2, new_size, '-'), sumOrSubMatrix(p3, p6, new_size, '+'), new_size, '+');
 
+    delete[] p1;
+    delete[] p2;
+    delete[] p3;
+    delete[] p4;
+    delete[] p5;
+    delete[] p6;
+    delete[] p7;
+	
     return collectMatrix(c11, c12, c21, c22, new_size);
 }
 
 int main()
 {
     srand(time(nullptr));
-    int a_r = 2, a_c = 3, b_r = a_c, b_c = 4;
+    int a_r = 128, a_c = 128, b_r = a_c, b_c = 128;
     int n = getNewDimension(a_r, a_c, b_r, b_c);
     double** a_input = new double* [a_r];
    
@@ -201,8 +235,7 @@ int main()
             a_input[i][j] = getRandNumber();
 		}
 	}
-    cout << "A" << endl;
-    printMatrix(a_input, a_r, a_c);
+    
     double** b_input = new double* [b_r];
     for (int i = 0; i < b_r; i++)
     {
@@ -212,14 +245,34 @@ int main()
             b_input[i][j] = getRandNumber();
         }
     }
-    cout << "B" << endl;
-    printMatrix(b_input, b_r, b_c);
 
     double** a = addition2SquareMatrix(a_input, n, a_r, a_c);
     double** b = addition2SquareMatrix(b_input, n, b_r, b_c);
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     double** result = multiply(a, b, n);
-    printMatrix(result, a_r, b_c);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    std::chrono::steady_clock::time_point strassen_begin = std::chrono::steady_clock::now();
     double** resultStrassen = multiplyStrassen(a, b, n);
-    printMatrix(resultStrassen, a_r, b_c);
+    std::chrono::steady_clock::time_point strassen_end = std::chrono::steady_clock::now();
+    
+
+	if (n <= 16)
+	{
+        cout << "A" << endl;
+		printMatrix(a_input, a_r, a_c);
+        cout << "B" << endl;
+		printMatrix(b_input, b_r, b_c);
+        printMatrix(result, a_r, b_c);
+        printMatrix(resultStrassen, a_r, b_c);
+	}
+    cout << boolalpha << twoMatrixEqual(result, resultStrassen, a_r, b_c);
+    cout << " " << chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms";
+	cout << " " << chrono::duration_cast<std::chrono::milliseconds>(strassen_end - strassen_begin).count() << "ms";
+    delete a_input;
+    delete b_input;
+    delete a;
+    delete b;
+    delete result;
+    delete resultStrassen;
 	return 0;
 }
