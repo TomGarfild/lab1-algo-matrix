b7a8653496f5bdfe6e2f1228805363b94a806e49
TomGarfild <safronuyk.allex73@gmail.com>
2021-11-08 22:18:39 +0200

    oleksii refactoring.

--- Lab1-algo-matrix/Lab1-algo-matrix/main.cpp
+++ Lab1-algo-matrix/Lab1-algo-matrix/main.cpp
@@ -5,12 +5,21 @@ using namespace std;
 const int LIMIT_WHEN_USE_MULTIPLY = 64;
 const double EPSILON = 1e-6;
 
+double getRandNumber()
+{
+	return rand() % 10;
+	//return (rand() % 100 + 1) / 10.0;
+}
+
 class Matrix {
 public:
 	int rows;
 	int columns;
 	double** table;
-	Matrix() {}
+	Matrix(): rows(0), columns(0), table(nullptr)
+	{
+	}
+
 	~Matrix() {
 		for (int i = 0; i < this->rows; i++)
 		{
@@ -21,6 +30,19 @@ public:
 	Matrix(int rows, int columns) {
 		this->columns = columns;
 		this->rows = rows;
+		this->table = new double* [this->rows];
+		for (int i = 0; i < this->rows; i++)
+		{
+			this->table[i] = new double[this->columns];
+			for (int j = 0; j < this->columns; j++)
+			{
+				this->table[i][j] = 0;
+			}
+		}
+	}
+	Matrix(int size) {
+		this->columns = size;
+		this->rows = size;
 		this->table = new double* [this->rows];
 		for (int i = 0; i < this->rows; i++)
 		{
@@ -45,7 +67,7 @@ public:
 		}
 
 	}
-	Matrix operator=(const Matrix& other) {
+	Matrix& operator=(const Matrix& other) {
 		this->rows = other.rows;
 		this->columns = other.columns;
 		this->table = new double* [this->rows];
@@ -61,7 +83,8 @@ public:
 		}
 		return *this;
 	}
-	Matrix operator+(const Matrix& other) {
+	Matrix operator+(const Matrix& other) const
+	{
 		Matrix result(other.rows, other.columns);
 		for (int i = 0; i < this->rows; i++)
 		{
@@ -72,7 +95,8 @@ public:
 		}
 		return result;
 	}
-	Matrix operator-(const Matrix& other) {
+	Matrix operator-(const Matrix& other) const
+	{
 		Matrix result(other.rows, other.columns);
 		for (int i = 0; i < this->rows; i++)
 		{
@@ -83,8 +107,9 @@ public:
 		}
 		return result;
 	}
-	Matrix operator*(const Matrix& other) {
-		Matrix result(other.rows, other.columns);
+	Matrix operator*(const Matrix& other) const
+	{
+		Matrix result(this->rows, other.columns);
 
 		for (int i = 0; i < this->rows; i++)
 		{
@@ -98,7 +123,8 @@ public:
 		}
 		return result;
 	}
-	Matrix operator*(double number) {
+	Matrix operator*(double number) const
+	{
 		Matrix result(this->rows, this->columns);
 		for (int i = 0; i < this->rows; i++)
 		{
@@ -109,7 +135,27 @@ public:
 		}
 		return result;
 	}
-	Matrix transpose() {
+	bool operator==(const Matrix& other) const {
+		if (this->columns != other.columns || this->rows != other.rows)
+		{
+			return false;
+		}
+		
+		for (int i = 0; i < this->rows; i++)
+		{
+			for (int j = 0; j < this->columns; j++)
+			{
+				if (fabs(this->table[i][j] - other.table[i][j]) > EPSILON)
+				{
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	Matrix transpose() const
+	{
 		Matrix result(this->columns, this->rows);
 		for (int i = 0; i < this->columns; i++)
 		{
@@ -120,27 +166,45 @@ public:
 		}
 		return result;
 	}
-	void print() {
-		for (int i = 0; i < this->rows; i++)
-		{
-			for (int j = 0; j < this->columns; j++)
-			{
-				cout << this->table[i][j] << " ";
-			}
-			cout << "\n";
-		}
-	}
 	void randomizer() {
 		
 		for (int i = 0; i < this->rows; i++)
 		{
 			for (int j = 0; j < this->columns; j++)
 			{
-				this->table[i][j] = rand() % 100;
+				this->table[i][j] = getRandNumber();
 			}
 		}
 	}
+	Matrix resize(const int new_rows, const int new_columns) const
+	{
+		Matrix result(new_rows, new_columns);
+		for (int i = 0; i < new_rows; i++)
+		{
+			for (int j = 0; j < new_columns; j++)
+			{
+				if (i < this->rows && j < this->columns)
+				{
+					result.table[i][j] = this->table[i][j];
+				}
+			}
+		}
+
+		return result;
+	}
 };
+ostream& operator<<(ostream& os, const Matrix& matrix)
+{
+	for (int i = 0; i < matrix.rows; i++)
+	{
+		for (int j = 0; j < matrix.columns; j++)
+		{
+			os << matrix.table[i][j] << " ";
+		}
+		os << "\n";
+	}
+	return os;
+}
 Matrix identity_matrix(int n) {
 	Matrix result(n, n);
 	for (int i = 0; i < n; i++)
@@ -150,43 +214,12 @@ Matrix identity_matrix(int n) {
 	return result;
 }
 
-bool twoMatrixEqual(double** a, double** b, int r, int c)
-{
-	for (int i = 0; i < r; i++)
-	{
-		for (int j = 0; j < c; j++)
-		{
-			if (fabs(a[i][j] - b[i][j]) > EPSILON)
-			{
-				return false;
-			}
-		}
-	}
-
-	return true;
-}
-
-void printMatrix(double** matrix, int r, int c = -1)
-{
-	if (c <= 0) c = r;
-	for (int i = 0; i < r; i++)
-	{
-		for (int j = 0; j < c; j++)
-		{
-			cout << matrix[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
-}
-
-double getRandNumber()
-{
-	return (rand() % 100 + 1) / 10.0;
-}
 
 int log2(int x) {
+	if (x < 1) return NAN;
+	if (x == 1) return 0;
 	int result = 1;
+	x--;
 	while ((x >>= 1) != 0) {
 		result++;
 	}
@@ -198,26 +231,8 @@ int getNewDimension(int asize_r, int asize_c, int bsize_r, int bsize_c)
 	return 1 << log2(max(max(asize_r, asize_c), max(bsize_r, bsize_c)));
 }
 
-double** addition2SquareMatrix(double** a, int size, int size_r, int size_c) {
-	double** result = new double* [size];
-	for (int i = 0; i < size; i++)
-	{
-		result[i] = new double[size];
-		for (int j = 0; j < size; j++)
-		{
-			result[i][j] = 0;
-		}
-	}
-
-	for (int i = 0; i < size_r; i++) {
-		for (int j = 0; j < size_c; j++) {
-			result[i][j] = a[i][j];
-		}
-	}
-	return result;
-}
-
-void splitMatrix(double** a, double** a11, double** a12, double** a21, double** a22, int size) {
+void splitMatrix(const Matrix& a, Matrix& a11, Matrix& a12, Matrix& a21, Matrix& a22) {
+	const int size = a.columns;
 	const int new_size = size >> 1;
 
 	for (int i = 0; i < size; i++) {
@@ -225,286 +240,88 @@ void splitMatrix(double** a, double** a11, double** a12, double** a21, double** 
 		{
 			if (i < new_size && j < new_size)
 			{
-				a11[i][j] = a[i][j];
+				a11.table[i][j] = a.table[i][j];
 			}
 			else if (i < new_size && j >= new_size)
 			{
-				a12[i][j - new_size] = a[i][j];
+				a12.table[i][j - new_size] = a.table[i][j];
 			}
 			else if (i >= new_size && j < new_size)
 			{
-				a21[i - new_size][j] = a[i][j];
+				a21.table[i - new_size][j] = a.table[i][j];
 			}
 			else
 			{
-				a22[i - new_size][j - new_size] = a[i][j];
+				a22.table[i - new_size][j - new_size] = a.table[i][j];
 			}
 		}
 	}
 }
 
-double** collectMatrix(double** a11, double** a12, double** a21, double** a22, int size) {
+Matrix collectMatrix(const Matrix& a11, const Matrix& a12, const Matrix& a21, const Matrix& a22) {
+	const int size = a11.columns;
 	const int new_size = size << 1;
-	double** a = new double* [new_size];
-	for (int i = 0; i < new_size; i++)
-	{
-		a[i] = new double[new_size];
-	}
+	Matrix a(new_size);
 
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++)
 		{
-			a[i][j] = a11[i][j];
-			a[i][j + size] = a12[i][j];
-			a[i + size][j] = a21[i][j];
-			a[i + size][j + size] = a22[i][j];
+			a.table[i][j] = a11.table[i][j];
+			a.table[i][j + size] = a12.table[i][j];
+			a.table[i + size][j] = a21.table[i][j];
+			a.table[i + size][j + size] = a22.table[i][j];
 		}
 	}
 	return a;
 }
 
-double** multiply(double** a, double** b, int n)
-{
-	double** result = new double* [n];
-	for (int i = 0; i < n; i++)
-	{
-		result[i] = new double[n];
-		for (int j = 0; j < n; j++)
-		{
-			result[i][j] = 0;
-			for (int k = 0; k < n; k++)
-			{
-				result[i][j] += a[i][k] * b[k][j];
-			}
-		}
-	}
-	return result;
-}
 
-double** sumOrSubMatrix(double** a, double** b, int size, char ar_operator)
-{
-	double** result = new double* [size];
-	for (int i = 0; i < size; i++)
-	{
-		result[i] = new double[size];
-		for (int j = 0; j < size; j++)
-		{
-			if (ar_operator == '+')
-			{
-				result[i][j] = a[i][j] + b[i][j];
-			}
-			else if (ar_operator == '-')
-			{
-				result[i][j] = a[i][j] - b[i][j];
-			}
-		}
-	}
-	return result;
-}
-
-double** multiplyStrassen(double** a, double** b, int size) {
+Matrix multiplyStrassen(const Matrix& a, const Matrix& b) {
+	const int size = getNewDimension(a.rows, a.columns, b.rows, b.columns);
 	if (size <= LIMIT_WHEN_USE_MULTIPLY) {
-		return multiply(a, b, size);
+		return a * b;
 	}
+	const int a_r = a.rows;
+	const int b_c = b.columns;
+	Matrix ra = a.resize(size, size);
+	Matrix rb = b.resize(size, size);
+	
 	const int new_size = size >> 1;
 
-	double** a11 = new double* [new_size];
-	double** a12 = new double* [new_size];
-	double** a21 = new double* [new_size];
-	double** a22 = new double* [new_size];
-	double** b11 = new double* [new_size];
-	double** b12 = new double* [new_size];
-	double** b21 = new double* [new_size];
-	double** b22 = new double* [new_size];
+	Matrix a11(new_size);
+	Matrix a12(new_size);
+	Matrix a21(new_size);
+	Matrix a22(new_size);
+	Matrix b11(new_size);
+	Matrix b12(new_size);
+	Matrix b21(new_size);
+	Matrix b22(new_size);
 
-	for (int i = 0; i < new_size; i++)
-	{
-		a11[i] = new double[new_size];
-		a12[i] = new double[new_size];
-		a21[i] = new double[new_size];
-		a22[i] = new double[new_size];
-		b11[i] = new double[new_size];
-		b12[i] = new double[new_size];
-		b21[i] = new double[new_size];
-		b22[i] = new double[new_size];
-	}
+	splitMatrix(ra, a11, a12, a21, a22);
+	splitMatrix(rb, b11, b12, b21, b22);
 
-	splitMatrix(a, a11, a12, a21, a22, size);
-	splitMatrix(b, b11, b12, b21, b22, size);
+	Matrix p1 = multiplyStrassen(a11 + a22, b11 + b22);
+	Matrix p2 = multiplyStrassen(a21 + a22, b11);
+	Matrix p3 = multiplyStrassen(a11, b12 - b22);
+	Matrix p4 = multiplyStrassen(a22, b21 - b11);
+	Matrix p5 = multiplyStrassen(a11 + a12, b22);
+	Matrix p6 = multiplyStrassen(a21 - a11, b11 + b12);
+	Matrix p7 = multiplyStrassen(a12 - a22, b21 + b22);
 
-	double** p1 = multiplyStrassen(sumOrSubMatrix(a11, a22, new_size, '+'), sumOrSubMatrix(b11, b22, new_size, '+'), new_size);
-	double** p2 = multiplyStrassen(sumOrSubMatrix(a21, a22, new_size, '+'), b11, new_size);
-	double** p3 = multiplyStrassen(a11, sumOrSubMatrix(b12, b22, new_size, '-'), new_size);
-	double** p4 = multiplyStrassen(a22, sumOrSubMatrix(b21, b11, new_size, '-'), new_size);
-	double** p5 = multiplyStrassen(sumOrSubMatrix(a11, a12, new_size, '+'), b22, new_size);
-	double** p6 = multiplyStrassen(sumOrSubMatrix(a21, a11, new_size, '-'), sumOrSubMatrix(b11, b12, new_size, '+'), new_size);
-	double** p7 = multiplyStrassen(sumOrSubMatrix(a12, a22, new_size, '-'), sumOrSubMatrix(b21, b22, new_size, '+'), new_size);
+	Matrix c11 = p1 + p4 + p7 - p5;
+	Matrix c12 = p3 + p5;
+	Matrix c21 = p2 + p4;
+	Matrix c22 = p1 - p2 + p3 + p6;
 
-	delete[] a11;
-	delete[] a12;
-	delete[] a21;
-	delete[] a22;
-	delete[] b11;
-	delete[] b12;
-	delete[] b21;
-	delete[] b22;
-
-	double** c11 = sumOrSubMatrix(sumOrSubMatrix(p1, p4, new_size, '+'), sumOrSubMatrix(p7, p5, new_size, '-'), new_size, '+');
-	double** c12 = sumOrSubMatrix(p3, p5, new_size, '+');
-	double** c21 = sumOrSubMatrix(p2, p4, new_size, '+');
-	double** c22 = sumOrSubMatrix(sumOrSubMatrix(p1, p2, new_size, '-'), sumOrSubMatrix(p3, p6, new_size, '+'), new_size, '+');
-
-	delete[] p1;
-	delete[] p2;
-	delete[] p3;
-	delete[] p4;
-	delete[] p5;
-	delete[] p6;
-	delete[] p7;
-
-	return collectMatrix(c11, c12, c21, c22, new_size);
+	Matrix result = collectMatrix(c11, c12, c21, c22);
+	result = result.resize(a_r, b_c);
+	
+	return result;
 }
 
 //Kolya's part
 
-//םו ענובא גזו
-double** transposeMatrix(double** a, int n) {
-	double** a_t = new double* [n];
-	for (int i = 0; i < n; i++)
-	{
-		a_t[i] = new double[n];
-		for (int j = 0; j < n; j++)
-		{
-			a_t[i][j] = a[j][i];
-		}
-	}
-	return a_t;
-}
-//םו ענובא
-void multiplyNumberOnMatrix(double number, double** a, int size) {
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			a[i][j] *= number;
-		}
-	}
-}
-
-void calculateB(double** e, double** b, int n) {
-	double** t = new double* [n];
-	for (int i = 0; i < n; i++)
-	{
-		t[i] = new double[n];
-		for (int j = 0; j < n; j++)
-		{
-			t[i][j] = e[i][j];
-		}
-		t[i][i]++;
-	}
-
-	double** result = multiplyStrassen(t, b, n);
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			b[i][j] = result[i][j];
-		}
-		delete[] t[i];
-		delete[] result[i];
-	}
-	delete[]t;
-	delete[]result;
-}
-
-void calculateE(double** a, double** b, double** e, int n) {
-	double** t = multiplyStrassen(b, a, n);
-	multiplyNumberOnMatrix(-1, t, n);
-
-	for (int i = 0; i < n; i++)
-	{
-		t[i][i]++;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			e[i][j] = t[i][j];
-		}
-		delete[] t[i];
-	}
-	delete[]t;
-}
-
-bool E_passCriterionOfSmallness(double** e, int n) {
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (e[i][j] > 0.0001) {
-				return true;
-			}
-		}
-	}
-	return false;
-}
-
-double** reverseMatrixByNewtonsMethod(double** a, int n) {
-	double** result = transposeMatrix(a, n);
-	double t1 = DBL_MIN, t2 = DBL_MIN, t;
-	double current_sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		current_sum = 0;
-		for (int j = 0; j < n; j++)
-		{
-			current_sum += a[i][j];
-		}
-		t1 = (t1 < current_sum) ? current_sum : t1;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		current_sum = 0;
-		for (int j = 0; j < n; j++)
-		{
-			current_sum += a[j][i];
-		}
-		t2 = (t2 < current_sum) ? current_sum : t2;
-	}
-	t = 1 / (t1 * t2);
-	multiplyNumberOnMatrix(t, result, n);
-	
-	double** e = new double* [n];
-	for (int i = 0; i < n; i++)
-	{
-		e[i] = new double[n];
-	}
-	do {
-		calculateE(a, result, e, n);
-		calculateB(e, result, n);
-		/*for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				cout << result[i][j] << " ";
-			}
-			cout << endl;
-		}
-		cout << endl;*/
-	} while (E_passCriterionOfSmallness(e, n));
-
-
-	for (int i = 0; i < n; i++)
-	{
-		delete[] e[i];
-	}
-	delete[] e;
-
-	return result;
-}
-
-bool E_passCriterionOfSmallness(Matrix e) {
+bool E_passCriterionOfSmallness(const Matrix& e) {
 	for (int i = 0; i < e.rows; i++)
 	{
 		for (int j = 0; j < e.columns; j++)
@@ -552,59 +369,38 @@ Matrix reverseMatrixByNewtonsMethod(Matrix A) {
 	return B;
 }
 
-int main()
+void test_strassen()
 {
-	srand(time(nullptr));
-	int a_r = 128, a_c = 128, b_r = a_c, b_c = 128;
-	int n = getNewDimension(a_r, a_c, b_r, b_c);
-	double** a_input = new double* [a_r];
+	int a_r = 1024, a_c = 1024, b_r = a_c, b_c = 1024;
+	Matrix a(a_r, a_c);
+	a.randomizer();
 
-	for (int i = 0; i < a_r; i++)
-	{
-		a_input[i] = new double[a_c];
-		for (int j = 0; j < a_c; j++)
-		{
-			a_input[i][j] = getRandNumber();
-		}
-	}
+	Matrix b(b_r, b_c);
+	b.randomizer();
 
-	double** b_input = new double* [b_r];
-	for (int i = 0; i < b_r; i++)
-	{
-		b_input[i] = new double[b_c];
-		for (int j = 0; j < b_c; j++)
-		{
-			b_input[i][j] = getRandNumber();
-		}
-	}
-
-	double** a = addition2SquareMatrix(a_input, n, a_r, a_c);
-	double** b = addition2SquareMatrix(b_input, n, b_r, b_c);
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	double** result = multiply(a, b, n);
+	Matrix result = a * b;
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 	std::chrono::steady_clock::time_point strassen_begin = std::chrono::steady_clock::now();
-	double** resultStrassen = multiplyStrassen(a, b, n);
+	Matrix resultStrassen = multiplyStrassen(a, b);
 	std::chrono::steady_clock::time_point strassen_end = std::chrono::steady_clock::now();
 
 
-	if (n <= 16)
+	if (a_r <= 16)
 	{
-		cout << "A" << endl;
-		printMatrix(a_input, a_r, a_c);
-		cout << "B" << endl;
-		printMatrix(b_input, b_r, b_c);
-		printMatrix(result, a_r, b_c);
-		printMatrix(resultStrassen, a_r, b_c);
+		cout << "A" << endl << a;
+		cout << "B" << endl << b;
+		cout << result << endl;
+		cout << resultStrassen << endl;
 	}
-	cout << boolalpha << twoMatrixEqual(result, resultStrassen, a_r, b_c);
+	cout << boolalpha << (result == resultStrassen);
 	cout << " " << chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "ms";
 	cout << " " << chrono::duration_cast<std::chrono::milliseconds>(strassen_end - strassen_begin).count() << "ms";
-	delete a_input;
-	delete b_input;
-	delete a;
-	delete b;
-	delete result;
-	delete resultStrassen;
+}
+
+int main()
+{
+	srand(time(nullptr));
+	test_strassen();
 	return 0;
 }
